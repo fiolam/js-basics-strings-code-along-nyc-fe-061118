5ca6fa57e11ca4c26af791d3aa5c81d55696502f
fiolam <fiola.mulliqi@wework.com>
2018-07-16 23:07:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Fiola Mulliqi';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-const excitedWelcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+  const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!';
