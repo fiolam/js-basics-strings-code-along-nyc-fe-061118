cd1e0c1601eee72e5d0a0e3ea66bdb520fb41c79
fiolam <fiola.mulliqi@wework.com>
2018-07-16 23:14:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Fiola Mulliqi';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+let shortGreeting='Welcome, ' + currentUser.charAt(0) + '!';
